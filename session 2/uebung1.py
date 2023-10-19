ae14d7fd109da41ca0410c7e70a960ef1c84067e
Felix <zapalot@gmx.net>
2023-10-19 09:57:17 +0200

    added exercise for second week

---
+++ session 2/uebung1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#1.1)Tabelle einlesen
+# schaut in der Datein "2_2_aepfel_birnen_tabellen.py", wie dort eine Tabelle eingelesen aus einer Datei eingelesen wird.
+# Lest mit dieser Methode die Tabelle "iris.txt" ein.
+
+
+
+
+#1.2)Spalten aus der Tabelle auswählen
+# schaut in der Datein "2_2_aepfel_birnen_tabellen.py", wie dort eine Spalte aus der Tabelle ausgewählt wird.
+# Speichert die Spalte "Petal.Width" in eine eigene Variable 
+
+
+#1.3) Daten darstellen als Histogramm:
+# macht ein Plot, welches die Verteilung der Zuckergehalte in einem Histogramm darstellt.
+
+
+
+
+#1.4) Reflektion:
+# Folgen die Zuckergehalte einer Normalverteilung? Auf was könnte die Form des Histogramm hindeuten?
+
+
+#1.5.) Darstellung von zwei Spalten als Scatterplot
+#  benutzt die matplotlib-funktion "scatter", um die Datenpunkte darzustellen.
+# Dabei soll "Sepal.Width" auf der x-Achse dargestellt werden, "Petal.Width" auf der y-Achse
+
+#was fällt euch auf?
+
+
+
+
+#1.6.) Darstellung mit labels / installation des "seaborn" pakets
+# Eigentlich hätten wir ja gerne ein Plot, in dem die Spezies farbig unterscheidbar sind und es eine Legende dazu gibt.
+# oder gleich so ein schickes "pair" Plot wie hier:
+#https://en.wikipedia.org/wiki/Iris_flower_data_set
+
+#Mit matplotlib geht das, ist aber ein Haufen Arbeit...
+# Einfacher geht es mit dem zusatzpaket "seaborn".
+# Um das zu installieren:
+# 1.6.1) Terminal aufmachen
+# 1.6.2) Environment aktivieren: "mamba activate hnee2023"
+# 1.6.3) Paket installieren: "mamba install seaborn"
+# 1.6.4) hier im Code weiterarbeiten:  "import seaborn as sns" und dann ein Beispiel aus https://seaborn.pydata.org/generated/seaborn.pairplot.html anpassen.
+
+
+#1.8 Mittelwerte ausrechnen
+# schaut euch in "2_2_aepfel_birnen_tabellen.py" an, wie dort Mittelwerte für Gruppen von Werten in einem Pandas-Dataframe ausgerechnet werden, und macht das genauso mit den Iris Daten
+
+
+
+
+
+
+
